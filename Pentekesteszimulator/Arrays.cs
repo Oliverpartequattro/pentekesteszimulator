26a74a8ec3126491032c4be1f3c52be95b15ef7e
Oliverpartequattro <roncz.oliver@students.jedlik.eu>
2023-12-06 11:02:00 +0100

    goto

--- Pentekesteszimulator/Arrays.cs
+++ Pentekesteszimulator/Arrays.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography.X509Certificates;//goto
 
 namespace Pentekesteszimulator
 {
